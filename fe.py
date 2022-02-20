df060fe7a775334b391867304a1b254716bced88
Jacob4Berk <jehrlichster@berkeleypayment.com>
2022-02-19 21:49:27 -0500

    year_week_numbers function

--- fe.py
+++ fe.py
@@ -25,3 +25,25 @@ def day_week_numbers(dates: pd.Series):
     day_weeks = (numbered_days / 7).apply(math.ceil)
 
     return day_weeks
+
+
+def year_week_numbers(weeks: pd.Series):
+    """
+    convert consecutive week numbers into year and week features, such that:
+    - each year has 52 weeks
+    - the last week number is the series provided will be week 52 of the last year
+
+    e.g. week number 106 will be week 2 of year 3
+
+    args:
+    weeks: pd.Series of integers, representing consecutive weeks
+
+    returns:
+    years: integers representing consecutive "years" of 52 weeks
+    year_weeks: integers representing weeks within said "years"
+    """
+
+    years = (weeks / 52).apply(math.ceil)
+    year_weeks = (weeks % 52).replace(0, 52)
+
+    return years, year_weeks
