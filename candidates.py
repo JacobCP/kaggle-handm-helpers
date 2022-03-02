3e16253c08a1446d444dd208df1e8d0833faf1ba
Jacob4Berk <jehrlichster@berkeleypayment.com>
2022-03-01 22:48:30 -0500

    add_features_to_candidates function

--- candidates.py
+++ candidates.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def create_candidates(transactions_df, customers_df, articles_df):
     """
     will return candidates in form customer_id_article_id
@@ -43,7 +46,7 @@ def create_candidates(transactions_df, customers_df, articles_df):
         ]
     )
     features["article_id"] = article_purchases_df
-    candidates = past_purchases_cand + popular_items_cand
+    candidates = list(set(past_purchases_cand + popular_items_cand))
 
     return candidates, features
 
@@ -53,3 +56,37 @@ def filter_candidates(candidates):
     manual filtering logic of candidates, to keep it from getting out of hand
     """
     return candidates
+
+
+def add_features_to_candidates(candidates, features, customers_df, articles_df):
+    """
+    expands candidates to have customer_id and article_id fields,
+    adds fields needed to merge in features,
+    and merges features in
+    """
+    candidates_df = pd.DataFrame({"customer_article": candidates})
+    candidates_df["customer_id"] = candidates_df["customer_article"].apply(
+        lambda x: x.split("_")[0]
+    )
+    candidates_df["article_id"] = candidates_df["customer_article"].apply(
+        lambda x: x.split("_")[1]
+    )
+
+    for col_name in features:
+        feature_df = features[col_name]
+
+        # add the key to our df so we can merge the features in
+        if col_name not in candidates_df:
+            if col_name in customers_df:
+                col_name_dict = customers_df.set_index("customer_id")[col_name]
+                candidates_df[col_name] = candidates_df["customer_id"].map(
+                    col_name_dict
+                )
+            elif col_name in articles_df:
+                col_name_dict = articles_df.set_index("article_id")[col_name]
+                candidates_df[col_name] = candidates_df["article_id"].map(col_name_dict)
+
+        # now we can add the features
+        candidates_df = candidates_df.merge(feature_df, how="left", on=col_name)
+
+    return candidates_df
