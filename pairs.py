db6b1a8a1623b82aba47a7f175a24b7d8bf0c16c
Jacob4Berk <jehrlichster@berkeleypayment.com>
2022-03-22 14:34:45 -0400

    second version of creating pairs

--- pairs.py
+++ pairs.py
@@ -1,6 +1,20 @@
+import pandas as pd
+import torch
+
+if torch.cuda.is_available():
+    import cudf  # type: ignore
+
+    gpu_available = True
+    pd_or_cudf = cudf
+else:
+    gpu_available = False
+    pd_or_cudf = pd
+
+
 def create_pairs(
     df,
     last_week_number,
+    pairs_per_item,
 ):
     # get only the columns we need
     df = df[["customer_id", "article_id", "week_number"]].copy()
@@ -22,26 +36,33 @@ def create_pairs(
     df = df[~(df["article_id"].isin(single_customer_articles))].copy()
 
     # drop duplicates
-    df, last_week_df = df.drop_duplicates(), last_week_df.drop_duplicates()
+    df, lw_df = df.drop_duplicates(), last_week_df.drop_duplicates()
 
     # FIND ITEMS PURCHASED TOGETHER
     vc = df.article_id.value_counts()
-    pairs = {}
+    pairs_dfs = []
     for j, i in enumerate(vc.index.values):
         if j % 50 == 0:
             print(j, ", ", end="")
+
         USERS = df.loc[df.article_id == i.item(), "customer_id"].unique()
-        vc2 = last_week_df.loc[
-            (last_week_df.customer_id.isin(USERS))
-            & (last_week_df.article_id != i.item()),
-            "article_id",
-        ]
-        if len(vc2) == 0:
+        row_filter = (lw_df.customer_id.isin(USERS)) & (lw_df.article_id != i.item())
+        pairs_df = lw_df.loc[row_filter, "article_id"]
+
+        if len(pairs_df) == 0:
             continue
+
         try:
-            vc2 = vc2.value_counts()
-            pairs[i.item()] = vc2.index[0]
+            pairs_df = pairs_df.value_counts().reset_index()[:pairs_per_item]
+            pairs_df.columns = ["pair_article_id", "customer_count"]
+            pairs_df["percent_customers"] = pairs_df["customer_count"] / len(USERS)
+            pairs_df["article_id"] = i.item()
+
+            pairs_dfs.append(pairs_df)
         except Exception as e:
             print(type(e), ":", e)
 
-    return pairs
+    # concatenate them all together
+    all_pairs_df = pd_or_cudf.concat(pairs_dfs).reset_index(drop=True)
+
+    return all_pairs_df
